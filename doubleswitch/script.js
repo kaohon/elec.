9efb6d41408c408f0ad91ca55a1f62056e15c1a8
kaohon <74817397+kaohon@users.noreply.github.com>
2023-11-13 08:19:39 +0000

    file system

---
+++ doubleswitch/script.js
@@ -0,0 +1,42 @@
+// 変数の初期化
+l1 = false;
+s1 = false;
+s2 = false;
+
+l1_element = document.getElementById("l1");
+
+// ボタンの定義
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+
+// ボタンのクリックイベントリスナーの設定
+button1.addEventListener("click", function () {
+  // ボタン1が押された時の処理
+  s1 = true;
+  if (s2 === true) {
+    l1 = true;
+    l1_element.classList.add("l1-enabled");
+  } else if (l1 === false) {
+    l1 = true;
+    l1_element.classList.add("l1-enabled");
+  } else if (l1 === true) {
+    l1 = false;
+    l1_element.classList.remove("l1-enabled");
+    l1_element.classList.add("l1-disabled");
+  }
+});
+button2.addEventListener("click", function () {
+  // ボタン2が押された時の処理
+  s2 = true;
+  if (s1 === true) {
+    l1 = true;
+    l1_element.classList.add("l1-enabled");
+  } else if (l1 === false) {
+    l1 = true;
+    l1_element.classList.add("l1-enabled");
+  } else if (l1 === true) {
+    l1 = false;
+    l1_element.classList.remove("l1-enabled");
+    l1_element.classList.add("l1-disabled");
+  }
+});
